280af45213a2865ff1ec9ca36a0a47d12c40db3c
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-06-27 14:43:54 +0200

    introduce a lib folder and default options

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const mongo = require('mongodb').MongoClient;
-const chance = new require('chance')();
-const _ = require('lodash');
+const tester = require('./lib/tester');
 
-const url = 'mongodb://localhost:27017/test_indexes';
-
-const exampleRecord = () => ({
-  name: chance.name(),
-});
-
-const query = { name: /^Br/ };
-
-mongo.connect(url)
-  .then((db) => {
-    console.log('Connection open');
-    const testCollection = db.collection('test_collection');
-    testCollection.remove({});
-    _.times(10000, () => { testCollection.insert(exampleRecord()); });
-    testCollection.createIndex({ name: 1 });
-    testCollection.find(query).explain({ verbose: 'executionStats' })
-      .then((results) => {
-        console.log('results', results);
-      });
-    db.close();
-  })
-  .catch(console.error);
+tester();
