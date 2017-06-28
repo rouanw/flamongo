9375d6d0b87fc89af498efc17106a80bd8b2ff40
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-06-28 16:25:43 +0200

    include count of rejected plans

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const tester = require('./lib/tester');
 
-const indexKeys = [{ name: 1 }];
-const queries = [{ name: /^J/, birthday: { $lte: Date() } }];
+const indexKeys = [{ name: 1 }, { birthday: 1 }];
+const queries = [{ name: /^J/, birthday: { $lte: Date() }, vegan: false, surname: { $nin: ['Smith'] } }];
 const schema = {
   name: 'name',
   vegan: 'bool',
   birthday: 'date',
+  surname: 'last',
 };
 tester(indexKeys, queries, schema, { verbose: true, stubData: true });
