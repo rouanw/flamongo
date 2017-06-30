27a3aee50c97059a5f8bcaa25640e2db3af83eca
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-06-30 13:31:11 +0200

    add cli script

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const _ = require('lodash');
+const tester = require('../lib/tester');
+
+const opts = _.omit(argv, '_');
+const { indexKeys, queries, schema } = require(path.join(process.cwd(), argv._[0]));
+tester(indexKeys, queries, schema, opts);
