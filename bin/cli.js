356ed3dcd5b19fc3c9242802de9d13d77f2b2172
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-08-06 06:37:23 +0200

    expose explain and best index functionality as separate commands

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,19 @@ const argv = require('minimist')(process.argv.slice(2));
 const _ = require('lodash');
 const flamongo = require('../lib');
 
+const getCommand = (argv) => {
+  const c = argv._[0];
+  if (flamongo[c]) {
+    return flamongo[c];
+  }
+  console.error(`${c} is not a valid flamongo command`);
+  process.exit(1);
+};
+
+const command = getCommand(argv);
+const input = argv._[1];
 const opts = _.omit(argv, '_');
-const { indexKeys, queries, schema } = require(path.join(process.cwd(), argv._[0]));
-flamongo({ indexKeys, queries, schema }, opts, console.log)
+
+const { indexKeys, queries, schema } = require(path.join(process.cwd(), input));
+command({ indexKeys, queries, schema }, opts, console.log)
   .catch(console.error);
