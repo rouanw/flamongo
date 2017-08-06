695e98941eb896ca1a01490b5f4518dcdbd5f301
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-08-06 05:49:41 +0200

    inject logging dependency

--- bin/cli.js
+++ bin/cli.js
@@ -6,4 +6,5 @@ const tester = require('../lib/tester');
 
 const opts = _.omit(argv, '_');
 const { indexKeys, queries, schema } = require(path.join(process.cwd(), argv._[0]));
-tester(indexKeys, queries, schema, opts);
+tester(indexKeys, queries, schema, opts, console.log)
+  .catch(console.error);
