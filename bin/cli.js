c2a53c849917f9176b9755702f0db204a3338cea
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-08-06 06:02:59 +0200

    expect an object as input for queries, keys and schema

--- bin/cli.js
+++ bin/cli.js
@@ -6,5 +6,5 @@ const flamongo = require('../lib');
 
 const opts = _.omit(argv, '_');
 const { indexKeys, queries, schema } = require(path.join(process.cwd(), argv._[0]));
-flamongo(indexKeys, queries, schema, opts, console.log)
+flamongo({ indexKeys, queries, schema }, opts, console.log)
   .catch(console.error);
